b61b01d406c35b1bfeabce40ab141b77874d82eb
Jeremy Hon <jeremy.hon.gy@gmail.com>
2018-09-29 22:08:46 +0800

    reset history

--- index.js
+++ index.js
@@ -1,10 +1,126 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+require("dotenv").config();
+var plivo = require("plivo");
+var express = require("express");
+var app = express();
+var env = require("./app.json");
+const { initializeSwitch } = require("./switch");
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set("port", process.env.PORT || 5000);
+app.use(express.static(__dirname + "/public"));
+
+const getUrl = path => env.url + path + "/";
+
+app.post("/demo", function(request, response) {
+  p.calls
+    .create("14849302202", "6588235544", getUrl("conference"))
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(err => console.log(err));
+  p.calls
+    .create("14849302202", "6596700794", getUrl("conference"))
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(err => console.log(err));
+  p.calls
+    .create("14849302202", "6596686612", getUrl("automated"))
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(err => console.log(err));
+});
+
+// endpoint to return conference info
+app.all("/conference/", function(request, response) {
+  var r = plivo.Response();
+  r.addSpeak(
+    "You are being connected to the emergency call. Please wait while the other party picks up."
+  );
+  var params = {
+    enterSound: "beep:2", // Used to play a sound when a member enters the conference
+    record: "true", // Option to record the call
+    action: env.url + "conf_action/", // URL to which the API can send back parameters
+    method: "GET", // method to invoke the action Url
+    callbackUrl: env.url + "conf_callback/", // If specified, information is sent back to this URL
+    callbackMethod: "GET" // Method used to notify callbackUrl
+  };
+
+  var conference_name = "demo"; // Conference Room name
+  r.addConference(conference_name, params);
+  console.log(r.toXML());
+
+  response.set({
+    "Content-Type": "text/xml"
+  });
+  response.end(r.toXML());
+});
+
+app.all("/automated/", function(request, response) {
+  var r = plivo.Response();
+  r.addSpeak(
+    "Your neighbour mister tan at unit number 02-03 needs help. Please assist him."
+  );
+  response.set({
+    "Content-Type": "text/xml"
+  });
+  response.end(r.toXML());
+});
+
+app.all("/conf_action/", function(request, response) {
+  var conf_name = request.param("ConferenceName");
+  var conf_uuid = request.param("ConferenceUUID");
+  var conf_mem_id = request.param("ConferenceMemberID");
+  var record_url = request.param("RecordUrl");
+  var record_id = request.param("RecordingID");
+
+  console.log(
+    "Conference Name : " +
+      conf_name +
+      " Conference UUID  : " +
+      conf_uuid +
+      " Conference Member ID : " +
+      conf_mem_id +
+      " Record Url : " +
+      record_url +
+      " Record ID : " +
+      record_id
+  );
+});
+
+app.all("/conf_callback/", function(request, response) {
+  var conf_action = request.param("ConferenceAction");
+  var conf_name = request.param("ConferenceName");
+  var conf_uuid = request.param("ConferenceUUID");
+  var conf_mem_id = request.param("ConferenceMemberID");
+  var call_uuid = request.param("CallUUID");
+  var record_url = request.param("RecordUrl");
+  var record_id = request.param("RecordingID");
+
+  console.log(
+    "Conference Action : " +
+      conf_action +
+      " Conference Name : " +
+      conf_name +
+      " Conference UUID  : " +
+      conf_uuid +
+      " Conference Member ID : " +
+      conf_mem_id +
+      " Call UUID : " +
+      call_uuid +
+      " Record Url : " +
+      record_url +
+      " Record ID : " +
+      record_id
+  );
+});
+
+// Outbound Conference
+var plivo = require("plivo");
+
+var p = new plivo.Client(
+  process.env.PLIVO_AUTH_ID,
+  process.env.PLIVO_AUTH_TOKEN
+);
+
+initializeSwitch(app);
---
+++ switch.js
@@ -0,0 +1,45 @@
+const START_VOICE_RECOGNITION = "START_VOICE_RECOGNITION";
+const VOICE_RECOGNITION_STARTED = "VOICE_RECOGNITION_STARTED";
+const END_VOICE_RECOGNITION = "END_VOICE_RECOGNITION";
+const VOICE_RECOGNITION_ENDED = "VOICE_RECOGNITION_ENDED";
+
+const initializeSwitch = app => {
+  const server = require("http").Server(app);
+  const io = require("socket.io")(server);
+
+  server.listen(process.env.PORT || 5000, function() {
+    console.log(`socket has started listening on ${process.env.PORT || 5000}`);
+  });
+
+  io.on("connection", function(socket) {
+    console.log("The IoT device is connected");
+
+    socket.on(START_VOICE_RECOGNITION, () => {
+      io.emit(START_VOICE_RECOGNITION);
+      console.log(`${START_VOICE_RECOGNITION}: START`);
+    });
+
+    socket.on(VOICE_RECOGNITION_STARTED, () => {
+      io.emit(VOICE_RECOGNITION_STARTED);
+      console.log(`${VOICE_RECOGNITION_STARTED}: STARTED`);
+    });
+
+    socket.on(END_VOICE_RECOGNITION, () => {
+      io.emit(END_VOICE_RECOGNITION);
+      console.log(`${END_VOICE_RECOGNITION}: END`);
+    });
+
+    socket.on(VOICE_RECOGNITION_ENDED, () => {
+      io.emit(VOICE_RECOGNITION_ENDED);
+      console.log(`${VOICE_RECOGNITION_ENDED}: ENDED`);
+    });
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+  });
+};
+
+module.exports = {
+  initializeSwitch
+};
